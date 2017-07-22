92283a3ceb6359b93d216f8f3e3ff5516a5220bf
dilliraja-coax <dilliraja.coax@gmail.com>
2017-07-22 10:52:00 +0530

    Chat component created

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './home.component';
 import { homerouting } from "app/pages/home/home.routing";
 import { AuthGuard } from "app/pages/main/auth.guard";
 import { HeaderComponent } from "app/pages/home/header/header.component";
+import { ChatComponent } from './chat/chat.component';
+import { ChattComponent } from './chatt/chatt.component';
 
 
 //import { MainModule } from "app/pages/main/main.module";
@@ -17,7 +19,7 @@ import { HeaderComponent } from "app/pages/home/header/header.component";
     CommonModule,
     homerouting,
   ],
-  declarations: [HomeComponent,HeaderComponent],
+  declarations: [HomeComponent,HeaderComponent, ChatComponent, ChattComponent],
   providers: [ MainService, AuthGuard]
 })
 export class HomeModule { }
